c20d9bacf3053c357ae1dd52b6a6916164c201b1
hensley01 <hhubert@roc-dev.com>
2020-03-20 14:20:38 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Beginnerscursus PHP</h1>
+<h3>Hensley Hubert</h3>
+
+
+<li>
+    <a href="inhoudsopgave.php">Hoofdstuk 1</a>
+</li>
+
+<?php
+$hello = "Hello";
+$world = "World";
+
+echo "<h2>$hello {$world}!";
+?>
+</body>
+</html>
