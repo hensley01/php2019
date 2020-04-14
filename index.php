5901a59b257699b1e082fe2145cacad2c38d7d43
hensley01 <hhubert@roc-dev.com>
2020-04-14 18:04:50 +0200

    Hoofdstuk 3 test push

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
 <body>
 <h1>Beginnerscursus PHP</h1>
 <h3>Hensley Hubert</h3>
+<?php
+$hello = "Hello";
+$world = "World";
 
+echo "<h2>$hello {$world}!";
+?>
 
 <li>
     <a href="h1/inhoudsopgave.php">Hoofdstuk 1</a>
@@ -18,12 +23,26 @@
 <li>
     <a href="h2/aantal.php">Hoofdstuk 2(variabelen begin PHP)</a>
 </li>
-
-<?php
-$hello = "Hello";
-$world = "World";
-
-echo "<h2>$hello {$world}!";
-?>
+<li>
+    <a href="fortienafbeeldingen.php">Hoofdstuk 3, 10 afbeeldingen</a>
+</li>
+<li>
+    <a href="fortienboom.php">Hoofdstuk 3, boom afbeeldingen</a>
+</li>
+<li>
+    <a href="kerstboom.php">Hoofdstuk 3, kerstboom/array</a>
+</li>
+<li>
+<a href="whiletofor.php">Hoofdstuk 3, hoppelepee opdracht(omzetten naar array)</a>
+</li>
+<li>
+    <a href="fortienafbeeldingen.php">Hoofdstuk 3, aap afbeeldingen groen/rood</a>
+</li>
+<li>
+    <a href="busreis.php">Hoofdstuk 3, busreis</a>
+</li>
+<li>
+    <a href="h3/zwemclubs.php">Hoofdstuk 3, zwemclubs</a>
+</li>
 </body>
 </html>
